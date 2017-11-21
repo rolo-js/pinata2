bc5468462c9e5a51c38a6d1eacd4017d401bc003
Rolo Rolo <rriosarias@gmail.com>
2017-11-20 21:04:13 -0600

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
+import { MnFullpageOptions, MnFullpageService } from 'ngx-fullpage';
 
 @Component({
   selector: 'pi-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pi';
+  @Output() public options: MnFullpageOptions = new MnFullpageOptions({
+    scrollingSpeed: 700,
+    navigation: true,
+    css3: true,
+    navigationPosition: 'right',
+    verticalCentered: false
+  })
+//   @Input() public options: MnFullpageOptions = new MnFullpageOptions({
+//     navigation: true,
+//     navigationPosition: 'right',
+//     keyboardScrolling: false,
+//     scrollingSpeed: 2000
+// });
+// constructor(public fullpageService: MnFullpageService) {
+// }
+
 }
